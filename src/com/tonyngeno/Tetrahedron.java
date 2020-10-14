d04e0d9fb4b5475a07f35f2a793eff38d749ba21
TonyNgeno <tonyngeno123@gmail.com>
2020-10-14 14:04:16 +0300

    initial commit

--- src/com/tonyngeno/Tetrahedron.java
+++ src/com/tonyngeno/Tetrahedron.java
@@ -6,14 +6,44 @@ import java.util.Scanner;
 
 public class Tetrahedron extends Shape implements ShapePI {
 
+    private Scanner scanner;
+    private double height;
 
-    @Override
-    public double calculateArea() {
-        return 0;
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Tetrahedron() {
+        scanner = new Scanner(System.in);
+        prompt(scanner);
     }
 
     @Override
-    public void prompt(Scanner scanner) {
+    public double calculateArea() {
+        return (2*(getHeight()*getHeight()+getWidth()*getWidth()+getHeight()*getHeight()));
+    }
 
+
+    @Override
+    public void prompt(Scanner scanner){
+        System.out.println("enter the Length  of the Tetrahedron");
+        setHeight(scanner.nextInt());
+        System.out.println("enter the Width  of the Tetrahedron");
+        setWidth(scanner.nextInt());
+        System.out.println("enter the Height  of the Tetrahedron");
+        setHeight(scanner.nextInt());
+    }
+
+    @Override
+    public String toString() {
+        return "Shape type =" + "Box" +
+                " | Length =" + getHeight() +
+                " | Width =" + getHeight() +
+                " | Height =" + getHeight() +
+                " | Area =" + calculateArea() ;
     }
 }
